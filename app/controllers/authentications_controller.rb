1422da5b2da8f29915c307819bbbe5eaf5c88911
Niall Paterson <niall@piruby.com>
2015-02-20 19:41:09 +0000

    tidied up authentications controller

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,23 +1,21 @@
 class AuthenticationsController < ApplicationController
-  def create
-    # either create from fb access token or email
-    if params[:fb_access_token]
-      user = User.auth_using_facebook_access_token params[:fb_access_token]
-      if !user
-        render json: {error: "Not a member of a valid university group"}
-      else
-        render json: {success: true}
-      end
-    elsif params[:email]
-      # no password as this is simply verification
-      # ensure valid access token first. TODO: tidy up?
-      check_authentication
-      if University.valid_email? params[:email]
-        @user.send_verification_email
-        render json: {success: true, message:"Email Verification sent"}
-      else
-        render json: {error: "Not a valid university email address"}
-      end
+  before_action :check_authentication, only: :email
+
+  def facebook
+    user = User.auth_using_facebook_access_token params[:fb_access_token]
+    if !user
+      render json: {error: "Not a member of a valid university group"}
+    else
+      render json: {success: true}
+    end
+  end
+
+  def email
+    if University.valid_email? params[:email]
+      @user.send_verification_email
+      render json: {success: true, message:"Email Verification sent"}
+    else
+      render json: {error: "Not a valid university email address"}
     end
   end
 end
